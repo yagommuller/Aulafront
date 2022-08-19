133e825822d770fe58665dc89336b24ea051d3b8
yagommuller <110571708+yagommuller@users.noreply.github.com>
2022-08-19 18:59:38 -0300

    Script de evento
    Tive algumas dificuldades para montar o script, ainda nao consegui resolver o final dele mais para frente em algumas aulas eu tento terminar

---
+++ aula.js
@@ -0,0 +1,15 @@
+const dataAtual = 25
+
+console.log ("Seja bem vindo, para continuar insira a data do evento")
+let dataDoEvento = 25
+if (dataDoEvento > dataAtual) {console.log("Evento confirmado")}
+
+console.log("confirme sua idade: ")
+let idadeReal = 29
+if (idadeReal > 18) {console.log("idade permitida")}
+else (console.log("Cadastro concluido!"))
+
+console.log("Numero de participantes")
+let NumeroDeParticipantes = 89
+if (participantes < 100) {console.log("quantidade de participantes permitida")}
+else (console.log ("Quantidade permitida, seu acesso esta liberado para dia 25"))
